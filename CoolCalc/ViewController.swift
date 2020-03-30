1c54e9277c04bd3e9e86b0af7d6d74a8a764a00a
Zach Cervi <zachcervi@gmail.com>
2020-03-29 21:09:03 -0600

    init commit

---
+++ CoolCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoolCalc
+//
+//  Created by Zach Cervi on 3/29/20.
+//  Copyright © 2020 Zach Cervi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
